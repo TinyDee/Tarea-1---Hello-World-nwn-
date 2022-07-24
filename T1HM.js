f608727ce05b4c64fa683fbe6ddc96a642e67219
TinyDee <109770709+TinyDee@users.noreply.github.com>
2022-07-24 16:49:25 -0400

    Actualizo T1HM

--- T1HM.js
+++ T1HM.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const path = require("path");
 
 const t1hm = express();
 
 t1hm.get("/",(req, res) => {
-    res.send("Hola Lic.!!! ✌(>‿◠)");
+    //res.send("Hola Lic.!!! ✌(>‿◠)");
+    res.sendfile(path.join(__dirname+'/Tabla.html'))
 });
 
 t1hm.listen(3001, ()=> {
